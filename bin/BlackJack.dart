849ad5d89b93daf0a8c98c646860eeb0871dd35f
KolbeinnNoi <kolbeinnnoi99@gmail.com>
2024-10-26 14:51:26 +0000

    Update BlackJack.dart

--- bin/BlackJack.dart
+++ bin/BlackJack.dart
@@ -1,10 +1,44 @@
-import 'package:BlackJack/functions/functions.dart';
 import 'package:BlackJack/lib.dart';
-
-/*
-  Can you puzzle the methods together to form the game?
- */
+import 'package:BlackJack/functions/functions.dart';
 
 void main() {
- print("hello");
+  Welcome();
+  
+  var bankRoll = BuyIn();
+  
+  var deck = ShuffledDeck();
+  var playerHand = <int>[];
+  var houseHand = <int>[];
+  
+  while (bankRoll > 0) {
+    print('\nCurrent bankroll: \$${bankRoll}');
+    var currentBet = PlaceBet(bankRoll);
+    
+    if (currentBet == 0) {
+      print('Thanks for playing!');
+      break;
+    }
+    
+    playerHand.clear();
+    houseHand.clear();
+    InitialDeal(playerHand, houseHand, deck);
+  
+    Status(playerHand, houseHand);
+    
+    HitOrStay(playerHand, houseHand, deck);
+    
+    if (!CheckIfBusted(playerHand)) {
+      HousePlays(houseHand, deck);
+    }
+    
+    bankRoll = CheckWinner(playerHand, houseHand, bankRoll, currentBet);
+    
+    ReturnHands(playerHand, houseHand, deck);
+    
+    if (deck.length < 20) {
+      deck = ShuffledDeck();
+    }
+  }
+  
+  print('Game over! Final bankroll: \$${bankRoll}');
 }
