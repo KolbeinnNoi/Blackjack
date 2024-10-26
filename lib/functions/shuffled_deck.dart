38ee17ec02b32e944e4e36471beabf93591e5da1
Sakacogu <sarakatrin772@gmail.com>
2024-10-26 12:13:13 +0000

    shuffle cards

--- lib/functions/shuffled_deck.dart
+++ lib/functions/shuffled_deck.dart
@@ -1,12 +1,11 @@
 import 'shuffler.dart';
 import 'deck_of_cards.dart';
-/*
-Create a function called ShuffledDeck that uses the methods from
 
-A new deck of cards
-Shuffler
-And when run, returns a shuffled deck of cards. No inputs, only output.
- */
+List<int> ShuffledDeck() {
+  List<int> deck = deckOfCards();
+  return Shuffler(deck);
+}
 
-List<int> ShuffledDeck(){
+void main() {
+  print(ShuffledDeck());
 }
