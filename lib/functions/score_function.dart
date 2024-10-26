5e5b53497488df372eca8af128f43bb7fd87c21b
KolbeinnNoi <kolbeinnnoi99@gmail.com>
2024-10-26 15:01:31 +0000

    Update score_function.dart

--- lib/functions/score_function.dart
+++ lib/functions/score_function.dart
@@ -1,22 +1,60 @@
-int CalculateScore(List<int> hand) {
-  int score = 0;
-  int aces = 0;
-  
-  for (int card in hand) {
-    if (card == 1) {
-      aces++;
+import 'card_namer.dart';
+import 'deck_of_cards.dart';
+/*
+Create a function called CalculateScore that can calculate the score of a given hand of cards.
+The score is blackjack score, so the following rules apply
+
+An ace can either be 1 or 11
+10, Jack, Queen and King count as 10
+
+For this score function, we will do it a bit differently than real life.
+
+If a card is an Ace, it's AUTOMATICALLY an 11. Any ace that comes after that
+gets the score 1. So if the method gets the hand [1, 1] the score would be 12
+
+  Here are few test cases to test your method against
+
+  hand = [1, 1] -> Score 12
+  hand = [13, 1] -> Score 21
+  hand = [1, 13, 1] -> Score 22
+  hand = [1, 1, 1, 1] -> Score 14
+  hand = [11, 13] -> Score 20
+  hand = [7, 1] -> Score 18
+
+ */
+
+
+
+int CalculateScore(List<int> hand){
+int score = 0;
+bool ace = false;
+
+
+for(int card in hand) {
+  if (card == 1) {
+    if(!ace) {
       score += 11;
-    } else if (card > 10) {
-      score += 10;
+      ace = true;
     } else {
-      score += card;
+      score += 1;
     }
-  }
-  
-  while (score > 21 && aces > 0) {
+  } 
+  else if (card >= 10) {
+    score += 10;
+  } else {
+    score += card;
+  } 
+  if (ace && score > 21) {
     score -= 10;
-    aces--;
   }
+}
   
-  return score;
+return score;
+}
+
+void main() {
+  List<int> hand = [1, 10];
+
+  int score = CalculateScore(hand);
+  print("The score of your hand is: $score");
 }
