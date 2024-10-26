8512d45c8726a6fa11320a3e4f7422d7ae3f01a0
KolbeinnNoi <kolbeinnnoi99@gmail.com>
2024-10-26 14:53:00 +0000

    Update card_namer.dart

--- lib/functions/card_namer.dart
+++ lib/functions/card_namer.dart
@@ -5,6 +5,19 @@ When given an integer between 1-13,
 it should return the name of the corresponding card (Ignore the suit)
  */
 
-String CardNamer(int i){
-return "";
+
+
+String CardNamer(int number) {
+  switch (number) {
+    case 1:
+      return 'Ace';
+    case 11:
+      return 'Jack';
+    case 12:
+      return 'Queen';
+    case 13:
+      return 'King';
+    default:
+      return number >= 2 && number <= 10 ? number.toString() : 'Invalid';
+  }
 }
