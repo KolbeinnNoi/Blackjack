90175c706b66ad4c5ffb8bcbe62eab4d5a73fcfa
KolbeinnNoi <kolbeinnnoi99@gmail.com>
2024-10-26 15:02:07 +0000

    Update shuffled_deck.dart

--- lib/functions/shuffled_deck.dart
+++ lib/functions/shuffled_deck.dart
@@ -1,5 +1,5 @@
-import 'shuffler.dart';
-import 'deck_of_cards.dart';
+import 'dart:math';
+import 'package:BlackJack/functions/functions.dart';
 /*
 Create a function called ShuffledDeck that uses the methods from
 
@@ -8,5 +8,16 @@ Shuffler
 And when run, returns a shuffled deck of cards. No inputs, only output.
  */
 
-List<int> ShuffledDeck(){
+List<int> ShuffledDeck() {
+  List<int> deck = deckOfCards();
+  Random random = Random();
+  
+  for (int i = deck.length - 1; i > 0; i--) {
+    int j = random.nextInt(i + 1);
+    int temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+  }
+  
+  return deck;
 }
