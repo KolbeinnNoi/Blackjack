b1783c66b52639637c972db4ac051d6552894629
Sakacogu <sarakatrin772@gmail.com>
2024-10-26 13:35:48 +0000

    Update return_hand.dart

--- lib/functions/return_hand.dart
+++ lib/functions/return_hand.dart
@@ -8,17 +8,17 @@ void ReturnHands(List<int> playerHand, List<int> houseHand, List<int> deck) {
 }
 
   void main() {
-    List<int> deck = [3, 4, 5, 6];
-    List<int> hand = [1, 2];
+  List<int> deck = [3, 4, 5, 6];
+  List<int> hand = [1, 2];
 
-    ReturnHand(hand, deck);
-    print(deck);
-    print(hand);
-    List<int> playerHand = [7, 8];
-    List<int> houseHand = [9, 10];
+  ReturnHand(hand, deck);
+  print(deck);
+  print(hand);
+  List<int> playerHand = [7, 8];
+  List<int> houseHand = [9, 10];
 
-    ReturnHands(playerHand, houseHand, deck);
-    print(deck);
-    print(playerHand);
-    print(houseHand);
+  ReturnHands(playerHand, houseHand, deck);
+  print(deck);
+  print(playerHand);
+  print(houseHand);
   }
