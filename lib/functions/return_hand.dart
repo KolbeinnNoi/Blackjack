41211153d77dc19e2320c038fbaee6755e25ebd1
Sakacogu <sarakatrin772@gmail.com>
2024-10-26 13:34:07 +0000

    Update return_hand.dart

--- lib/functions/return_hand.dart
+++ lib/functions/return_hand.dart
@@ -1,26 +1,24 @@
-/*
-Create a function that is capable of accepting two lists of integers,
-empty the content of one list to the end of the other list, call it ReturnHand.
-
-List<int> deck = [3, 4, 5, 6];
-List<int> hand= [1, 2];
-ReturnHand(hand, deck);
-
-print(deck); // Should print [3, 4, 5, 6, 1, 2]
-print(hand); // Should print []
-
- */
-
-
 void ReturnHand(List<int> hand, List<int> deck){
-
+  deck.addAll(hand);
+  hand.clear();
+}
+void ReturnHands(List<int> playerHand, List<int> houseHand, List<int> deck) {
+  ReturnHand(playerHand, deck);
+  ReturnHand(houseHand, deck);
 }
 
-/*
-Then create another function called ReturnHands that uses the above function
-and returns both houseHand and playerHand into the deck.
- */
+  void main() {
+    List<int> deck = [3, 4, 5, 6];
+    List<int> hand = [1, 2];
 
-void ReturnHands(List<int> playerHand, List<int> houseHand, List<int> deck){
+    ReturnHand(hand, deck);
+    print(deck);
+    print(hand);
+    List<int> playerHand = [7, 8];
+    List<int> houseHand = [9, 10];
 
-}
+    ReturnHands(playerHand, houseHand, deck);
+    print(deck);
+    print(playerHand);
+    print(houseHand);
+  }
