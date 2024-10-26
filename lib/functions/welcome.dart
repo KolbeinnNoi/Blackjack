bf28bb6d8446b2ab94ad2a8be21b87a384fa0e3b
KolbeinnNoi <kolbeinnnoi99@gmail.com>
2024-10-26 15:04:49 +0000

    Update welcome.dart

--- lib/functions/welcome.dart
+++ lib/functions/welcome.dart
@@ -8,6 +8,12 @@ The only rule is that it should be a function
 that has no input and simply prints a text.
 */
 
-void Welcome(){
-
+void Welcome() {
+  print('''
+  ♠♥♣♦ LUCK DARTS -S BlackJack! ♦♣♥♠
+  
+  Try your luck and beat the house!
+  Remember: Get as close to 21 as possible without going over.
+  Good luck and have fun!
+  ''');
 }
